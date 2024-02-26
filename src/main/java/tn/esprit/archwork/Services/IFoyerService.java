e535004b83b597af625cd873b1b188dc2c04612b
dhia1195 <meddhiadin.boudali@esprit.tn>
2024-02-26 10:28:04 +0100

    dhia

---
+++ src/main/java/tn/esprit/archwork/Services/IFoyerService.java
@@ -0,0 +1,24 @@
+package tn.esprit.archwork.Services;
+
+import tn.esprit.archwork.entities.Bloc;
+import tn.esprit.archwork.entities.Foyer;
+
+import java.util.List;
+
+public interface IFoyerService {
+    List<Foyer> retrieveAllFoyers();
+
+    Foyer addFoyer (Foyer f);
+
+    Foyer updateFoyer (Foyer f);
+
+    Foyer retrieveFoyer (long  idFoyer);
+
+    void removeFoyer (long idFoyer);
+    public Foyer ajouterFoyerEtAffecterAUniversite (Foyer foyer, long idUniversite) ;
+
+    List<Foyer> findFoyersByUniversiteIsNull();
+
+
+
+}
