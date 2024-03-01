360431da1b1dc94a68a6f4cae8bbbbb1ab93bab9
Dhia <80337634+dhia1195@users.noreply.github.com>
2024-03-01 23:20:10 +0100

    Delete src/main directory

--- src/main/java/tn/esprit/archwork/Respositories/UniversiteRepo.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.archwork.Respositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.archwork.entities.Universite;
-
-public interface UniversiteRepo extends JpaRepository<Universite,Long> {
-    Universite findUniversiteByNomUniv(String nomUniversite);
-
-}
--- src/main/java/tn/esprit/archwork/entities/TypeChambre.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.archwork.entities;
-
-public enum TypeChambre {
-    SIMPLE,DOUBLE,TRIPLE
-}
